09eae4a4918e3669ed11fcf77d0705d5b7a81033
HenryDukuye <dukeweb.ng@gmail.com>
2020-04-24 23:58:18 +0100

    new commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const todos = [
+    {
+        id:1,
+        text: 'i am ready for the answer',
+        isComplete: true
+    },
+    {
+        id:2,
+        text: 'i will be successful ',
+        isComplete: true
+    },
+    {
+        id:3,
+        text: 'Learnt laracast to section 20',
+        isComplete: false
+    },
+]
+const jsonTodo = todos
+// console.log(JSON.stringify(todos));
+// for (i = 0 ; i< todos.length ; i++){
+//     console.log(todos[i]);
+// }
+
+// for (let todo of todos){
+//     console.log(todo.text);
+//
+// }
+
+// todos.forEach(function (todo) {
+//     console.log(todo);
+// });
+
+// const todotext= todos.map(function (todo) {
+//     return todo.text;
+// });
+
+// const todoComplete= todos.filter(function (todo) {
+//    return todo.isComplete===true;
+// }).map(function (todo) {
+//    return todo.text;
+// });
+//
+// console.log(JSON.stringify(todoComplete));
+
+//     const x = 100
+// if (x === 100){
+//     console.log('My guy X 100');
+// } else if (x > 100){
+//     console.log('X is greater than 10')
+// }
+// else {
+//     console.log('X is less than 10')
+// }
+
+
+
+const x = 13
+const color = x >10 ? 'red' :'blue'
+
+console.log(color);
+
